572e2511fc903f6ed98e90f40bcc3d5a5f12b816
Enrique92 <enrique.alcaide92@gmail.com>
2018-10-24 14:49:57 -0700

    Add the menu class

---
+++ src/main/java/Menu.java
@@ -0,0 +1,12 @@
+/**
+ * Created by administrator on 2018-10-24.
+ */
+public class Menu {
+
+
+    @Override
+    public String toString() {
+        return "------ Contact App ------\n" +
+                "1. Create a new contact.";
+    }
+}
