a8914e9a4797100b873fd00f139568e4ee349952
Enrique92 <enrique.alcaide92@gmail.com>
2018-10-24 14:41:30 -0700

    Add the class Menu and Driver

---
+++ src/main/java/Menu.java
@@ -0,0 +1,12 @@
+/**
+ * Created by administrator on 2018-10-24.
+ */
+public class Menu {
+    private int option;
+
+    @Override
+    public String toString() {
+        return ("----- Contact App -----");
+    }
+
+}
