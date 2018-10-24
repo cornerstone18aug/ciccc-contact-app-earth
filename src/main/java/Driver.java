eaaa4897554b7b58d81cf717c96b5eb29406f259
Enrique92 <enrique.alcaide92@gmail.com>
2018-10-24 14:57:26 -0700

    Update Driver and Menu

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,5 +2,9 @@
  * Created by administrator on 2018-10-24.
  */
 public class Driver {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        System.out.println(menu);
+    }
 
 }
--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -3,10 +3,20 @@
  */
 public class Menu {
 
+    private int opt;
+
+    public String option(int opt) {
+        return "";
+    }
+
 
     @Override
     public String toString() {
-        return "------ Contact App ------\n" +
-                "1. Create a new contact.";
+        return "------------------------------\n" +
+               "--------- Contact App --------\n" +
+               "-- 1. Create a new contact ---\n" +
+               "-- 2. List all the contacts --\n" +
+               "---------- 3. Exit -----------\n" +
+               "------------------------------";
     }
 }
