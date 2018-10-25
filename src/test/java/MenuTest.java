2edc91e984f511ee62fe1dbb48ac3514b2811f10
nator333 <m.nakamata35@gmail.com>
2018-10-25 00:31:48 -0700

    Add test code

---
+++ src/test/java/MenuTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MenuTest {
+
+  private static final List<Contact> contacts = new ArrayList<>();
+  private static final Contact derickContact = new Contact(
+      0,
+      "Derrik",
+      "Park",
+      "dwrer3454re",
+      "r5fdt5er",
+      "rt5rtyg",
+      "aaa@gmail.com");
+  private Menu menu;
+
+  @Before
+  public void setUp() {
+    contacts.add(derickContact);
+    this.menu = new Menu(contacts);
+  }
+
+  @Test
+  public void execute() {
+  }
+
+  @Test
+  public void addUser() {
+  }
+
+  @Test
+  public void checkEmail() {
+    assertFalse(menu.checkEmail("aaa@gmail.com"));
+    assertTrue(menu.checkEmail("bbb@gmail.com"));
+  }
+
+  @Test
+  public void checkPhones() {
+    assertTrue(menu.checkPhones("23457809"));
+    assertFalse(menu.checkPhones("2-3457809"));
+  }
+}
